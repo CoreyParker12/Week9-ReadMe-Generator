9f20772ab882b139bb464f679c7c9c8b58552189
Corey Parker <coreyparker012@gmail.com>
2021-06-17 19:42:54 -0400

    added question function/called init to test functionality

--- index.js
+++ index.js
@@ -1,57 +1,67 @@
-// TODO: Include packages needed for this application
+// Packages
 
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [
-        {
-            type: 'input',
-            name: 'Project Title',
-            message: 'Project title? ',
-        },
-        {
-            type: 'input',
-            name: 'Project Description',
-            message: 'Project description? ',
-        },
-        {
-            type: 'input',
-            name: 'ToC',
-            message: 'Table of Contents? ',
-        },
-        {
-            type: 'input',
-            name: 'Installation',
-            message: 'What are the installation instructions? ',
-        },
-        {
-            type: 'input',
-            name: 'Usage',
-            message: 'How will this project be used? ',
-        },
-        {
-            type: 'input',
-            name: 'License',
-            message: 'Please select which license you are using:',
-        },
-        {
-            type: 'input',
-            name: 'Contributing',
-            message: 'What are the contribution guidelines?',
-        },
-        {
-            type: 'input',
-            name: 'Tests',
-            message: 'What are the test instructions?',
-        },
-];
+// Array of questions to be passed to the user
+
+const questions = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'Project Title',
+                message: 'Project title? ',
+            },
+            {
+                type: 'input',
+                name: 'Project Description',
+                message: 'Project description? ',
+            },
+            {
+                type: 'input',
+                name: 'ToC',
+                message: 'Table of Contents? ',
+            },
+            {
+                type: 'input',
+                name: 'Installation',
+                message: 'What are the installation instructions? ',
+            },
+            {
+                type: 'input',
+                name: 'Usage',
+                message: 'How will this project be used? ',
+            },
+            {
+                type: 'input',
+                name: 'License',
+                message: 'Please select which license you are using:',
+            },
+            {
+                type: 'input',
+                name: 'Contributing',
+                message: 'What are the contribution guidelines?',
+            },
+            {
+                type: 'input',
+                name: 'Tests',
+                message: 'What are the test instructions?',
+            },
+        ]);
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => 
+        err ? console.log(err) : console.log('Successfully created README!')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions();
+}
 
 // Function call to initialize app
 init();
